66b4bf0c2ae586e567c1167e991b48cbd1578ca3
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2021-01-01 23:07:18 +0100

    home page styling

--- beerpongchampions/championship/models.py
+++ beerpongchampions/championship/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Player(models.Model):
+    Level = (
+        ('Amateur', 'Amateur'),
+        ('Intermediate', 'Intermediate'),
+        ('Professional', 'Professional'),
+        ('World Class', 'World Class'),
+        ('Champion', 'Champion'),
+    )
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    points = models.FloatField(null=True)
+    player_level = models.CharField(max_length=200, null=True, choices=Level)
+    profile_pic = models.ImageField(default="profile_pic_default.png", null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
--- beerpongchampions/championship/views.py
+++ beerpongchampions/championship/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def leaderboard(request):
+    context = {}
+    return render(request, 'championship/leaderboard.html', context)
+
+def home(request):
+    context = {}
+    return render(request, 'championship/home.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'championship/login.html', context)
