933f92a975e1884c65202e6b559f74856efd8da9
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2021-02-09 17:43:58 +0100

    register page

--- beerpongchampions/championship/views.py
+++ beerpongchampions/championship/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 from .models import *
+from .forms import CreateUserForm
+from django.contrib import messages
 
 def leaderboard(request):
     context = {}
@@ -25,3 +27,17 @@ def loginPage(request):
 
     context = {}
     return render(request, 'championship/login.html', context)
+
+def registerPage(request):
+    form = CreateUserForm
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'championship/register.html', context)
